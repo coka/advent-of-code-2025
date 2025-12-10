dd2a3ef606d0c1c9ef55dff22dc6a01094398631
Bojan Čoka <bojan.coka@gmail.com>
2025-12-10 13:19:26 +0100

    Day 2: Gift Shop

---
+++ 02_gift_shop.py
@@ -0,0 +1,39 @@
+from typing import Iterable
+
+
+def parse(input: str) -> Iterable[Iterable[int]]:
+    return [
+        range(int(start), int(end) + 1)
+        for start, end in (sequence.split("-") for sequence in input.split(","))
+    ]
+
+
+with open("02.txt") as f:
+    ranges = parse(f.read().strip())
+
+answer = 0
+for r in ranges:
+    for n in r:
+        n_str = str(n)
+        digits = len(n_str)
+        if digits % 2 == 1:
+            continue
+        half = digits // 2
+        if n_str[:half] == n_str[half:]:
+            answer += n
+
+answer2 = 0
+for r in ranges:
+    for n in r:
+        n_str = str(n)
+        digits = len(n_str)
+        for divisor in range(1, digits):
+            if digits % divisor == 0:
+                part = n_str[:divisor]
+                if part * (digits // divisor) == n_str:
+                    answer2 += n
+                    break
+
+
+print(answer)  # 23701357374
+print(answer2)  # 34284458938
